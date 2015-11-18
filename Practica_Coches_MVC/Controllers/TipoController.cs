6f1c22df9091f0b538f0788d0f95db5d1f0c694f
Alejandrogf <alejandrogarciaf@gmail.com>
2015-11-18 14:24:12 +0100

    MVC Parque Motor

---
+++ Practica_Coches_MVC/Controllers/TipoController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practica_Coches_MVC.Models;
+
+namespace Practica_Coches_MVC.Controllers
+{
+    public class TipoController : Controller
+    {
+        //Conexión a la BD
+        ParqueMotorEntities db = new ParqueMotorEntities();
+
+
+        // GET: Tipo
+        public ActionResult Index()
+        {
+            var data = db.Tipo;
+            return View(data);
+        }
+
+        // GET: Producto
+        public ActionResult Alta()
+        {
+            return View(new Tipo());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Tipo modeloTipo)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Tipo.Add(modeloTipo);
+                db.SaveChanges();
+                ModelState.Clear();
+                return View(new Tipo());
+            }
+
+            return View(modeloTipo);
+        }
+
+        //Si no se usa AJAX, el modificar se hace en dos pasos. Por eso hay dos instrucciones.
+        public ActionResult EditarTipo(int idTipo)
+        {
+            var data = db.Tipo.Find(idTipo);
+            return View(data);
+        }
+
+        [HttpPost]
+        //Esta linea es para añadir un sistema de seguridad (tipo captcha pero invisible)
+        [ValidateAntiForgeryToken]
+        public ActionResult EditarTipo(Tipo objetoTipo)
+        {
+            if (ModelState.IsValid)
+            {
+                //Al indicar que el estado del objeto es modificado, el programa por si solo
+                //ya sabe que hacer, buscando el objeto que coincida con los datos que tiene
+                //y actualiza el solo. Otra opción sería actualizar a mano, campo a campo.
+                //En este caso no hay problema porque es poco, pero con una tabla de 200 campos
+                //es un horror.
+                db.Entry(objetoTipo).State = EntityState.Modified;
+                db.SaveChanges();
+                //Con esto se redirige a la acción que queremos que se lleve a cabo.
+                return RedirectToAction("Index");
+            }
+            return View(objetoTipo);
+        }
+
+        public ActionResult VerVehiculos(int id)
+        {
+
+            ViewBag.vehiculo = db.Vehiculo.Where(o=>o.Tipo==id).ToList();
+            
+            ViewData["idTipo"] = id;
+            ViewData["nombreTipo"] = db.Tipo.Find(id).Nombre;
+            return  View();
+        }
+    }
+}
---
+++ Practica_Coches_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica_Coches_MVC.MvcApplication" Language="C#" %>
