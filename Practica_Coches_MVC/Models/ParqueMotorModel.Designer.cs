3a7924f528e7935668e9fb9cf6b0b4ed02f362df
Alejandrogf <alejandrogarciaf@gmail.com>
2015-11-24 14:30:05 +0100

    nueva db

--- Practica_Coches_MVC/Models/ParqueMotorModel.Designer.cs
+++ Practica_Coches_MVC/Models/ParqueMotorModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\alumno\documents\visual studio 2015\Projects\Practica_Coches_MVC\Practica_Coches_MVC\Models\ParqueMotorModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Alumno\documents\visual studio 2015\Projects\Practica_Coches_MVC\Practica_Coches_MVC\Models\ParqueMotorModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
